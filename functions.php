699d73d05b58f266553f3be535a1d94e3d91b5b7
sys7375 <juliobenin@yahoo.com.br>
2020-07-10 11:06:33 -0300

    configurando charset utf-8

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ session_start();
 
 if($_SESSION['usuario'] != null && $_SESSION['senha'] != null && $_SESSION['id'] != null){
     $con = new mysqli("localhost",'indexerpcom_catavento','HD,[98i3(3oC',"indexerpcom_catavento");
+    $con->set_charset("utf8");
     $resp = $con->query('select id from tbl_usuario where usuario = "'.$_SESSION['usuario'].'" and senha = "'.$_SESSION['senha'].'" and id = "'.$_SESSION['id'].'"');
     if($resp->num_rows <> 1){
         echo '<script>location.href="login.php?e"</script>';
