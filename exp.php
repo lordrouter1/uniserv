042ede31a09e1553e6ee8995aa65e49a450192c0
sys7375 <juliobenin@yahoo.com.br>
2020-09-10 11:12:03 -0300

    removido mensagens de erro

--- exp.php
+++ exp.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors',1);
-ini_set('display_startup_erros',1);
-error_reporting(E_ALL);
 
 if(isset($_GET['tbl'])){
     $con = new mysqli("localhost","uniserve","Ag147258@","data_uniserve");
