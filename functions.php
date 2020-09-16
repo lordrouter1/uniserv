0a4d4cad2b15fbee5941f66a862638cfb3c14335
juliobenin@yahoo.com.br <juliobenin@yahoo.com.br>
2020-09-16 15:28:33 -0300

    corrigido primeiro login

--- functions.php
+++ functions.php
@@ -9,18 +9,6 @@ session_start();
 
 require_once('con.php');
 
-if(!isset($_COOKIE['empresa'])){
-    $resp = $con->query('select id from tbl_configuracao where id in (select valor from tbl_usuarioMeta where meta = "habilitar_empresa" and usuario = '.$_SESSION['id'].')')->fetch_assoc();
-    setcookie('empresa',$resp['id']);
-    $_COOKIE['empresa'] = $resp['id'];
-}
-
-require_once('core/lib/juno/class.php');
-$resp = $con->query('select token,pagamentoStatus from tbl_configuracao where id = '.$_COOKIE['empresa'])->fetch_assoc();
-if($resp['pagamentoStatus'] == '1'){
-    $juno->loadRecipientToken($resp['token']);
-}
-
 if($_SESSION['usuario'] != null && $_SESSION['senha'] != null && $_SESSION['id'] != null){
     $con->set_charset("utf8");
     $resp = $con->query('select id from tbl_usuario where usuario = "'.$_SESSION['usuario'].'" and senha = "'.$_SESSION['senha'].'" and id = "'.$_SESSION['id'].'"');
@@ -38,6 +26,20 @@ function redirect($resp){
     }
     else{
         echo "<script>location.href='?e'</script>";
+    }
+}
+
+if(isset($_SESSION['id'])){
+    if(!isset($_COOKIE['empresa'])){
+        $resp = $con->query('select id from tbl_configuracao where id in (select valor from tbl_usuarioMeta where meta = "habilitar_empresa" and usuario = '.$_SESSION['id'].')')->fetch_assoc();
+        setcookie('empresa',$resp['id']);
+        $_COOKIE['empresa'] = $resp['id'];
+    }
+
+    require_once('core/lib/juno/class.php');
+    $resp = $con->query('select token,pagamentoStatus from tbl_configuracao where id = '.$_COOKIE['empresa'])->fetch_assoc();
+    if($resp['pagamentoStatus'] == '1'){
+        $juno->loadRecipientToken($resp['token']);
     }
 }
 
