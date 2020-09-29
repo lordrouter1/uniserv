8c1b8f7990e0886218c4913ac342c7c89492a9b6
juliobenin@yahoo.com.br <juliobenin@yahoo.com.br>
2020-09-29 18:05:55 -0300

    teste cadastro produção

--- core/ajax/configuracao/ativarJuno.php
+++ core/ajax/configuracao/ativarJuno.php
@@ -51,6 +51,8 @@
                     
                     $jResp = $juno->account($conta);
 
+                    var_dump($jResp);
+
                     if(isset($jResp->resourceToken)){
                         $con->query('update tbl_configuracao set pagamentoStatus = 1, token = "'.$jResp->resourceToken.'" where id = '.$_POST['empresa']);
                         echo 'true';
